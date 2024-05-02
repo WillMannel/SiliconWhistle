bba1ed864870107f792ef216eb06f4a3a7323ea0
Will Mannel <willmannel@gmail.com>
2024-05-02 14:50:54 -0500

    fixed file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ComplaintForm from './ComplaintForm';
+
+function App() {
+  return (
+    <div>
+      <h1>Anonymous Complaint Submission</h1>
+      <ComplaintForm />
+    </div>
+  );
+}
+
+export default App;
